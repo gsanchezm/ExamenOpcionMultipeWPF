88cfb707bad8b4d3dc6d0bcd92794807879ccf65
gilberto.asprosgmail.com <gilberto.aspros@gmail.com>
2019-09-19 11:45:54 -0500

    Added functionalito to insert data into access DB

---
+++ ConnectionDB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using System.Data.OleDb;
+
+namespace PrimerParcial
+{
+    public static class ConnectionDB
+    {
+        //private static OleDbConnection conn = new OleDbConnection();
+
+        public static string StrConnection()
+        {
+           // OleDbConnection conn = new OleDbConnection();
+            return new OleDbConnection().ConnectionString = ConfigurationManager.ConnectionStrings["Connection"].ToString();            
+        }
+
+        // abrir conexión a Base de Datos de Access
+        public static void ConnectToDataBase(OleDbConnection conn)
+        {
+            DisconnectDataBase(conn);
+            conn.Open();
+        }
+
+        // cerrar conexión a Base de Datos de Access
+        public static void DisconnectDataBase(OleDbConnection conn)
+        {
+            if (conn !=null && conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+        // Query para insertar datos
+        public static OleDbCommand InsertCalificacionAlumno(OleDbConnection conn)
+        {
+            ConnectionDB.ConnectToDataBase(conn);
+            return new OleDbCommand("INSERT INTO Calificaciones ([Carrera],[Nombre],[Id],[Calificacion],[Fecha]) values (@Carrera,@Nombre,@Id,@Calificacion,@Fecha)", conn);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
 
 namespace PrimerParcial
 {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        OleDbConnection conn = new OleDbConnection(ConnectionDB.StrConnection());
+        private int aciertos = 0;
+        private int errores = 0;
+        public int Aciertos { get => aciertos; set => aciertos = value; }
+        public int Errores { get => errores; set => errores = value; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnCalificar_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (StackPanel s in FindVisualChildren<StackPanel>(window))
+            {
+                foreach (RadioButton rb in s.Children)
+                {
+                    if (rb.Name.Contains("Correct") && rb.IsChecked == true)
+                    {
+                        Aciertos++;
+                    }
+                    else
+                    {
+                        Errores++;
+                    }
+                }
+            }
+
+            MessageBox.Show($"Obtuviste {Aciertos}! respuestas bien y {Errores}! respuestas mal", "Resultado", MessageBoxButton.OK, MessageBoxImage.Information);
+            
+            //Mensaje de la calificación obtenida
+            MessageBox.Show((Aciertos > 7 ? $"Tu calificación es de {Aciertos}!, pasaste de forma satisfactoria" : $"Tu calificación es de {Aciertos}!, lamentablemente no pasaste"), "Calificacion", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
+            try
+            {
+                OleDbCommand cmd = ConnectionDB.InsertCalificacionAlumno(conn);
+
+                // Agregando valores del examen
+                cmd.Parameters.AddWithValue("@Carrera", txtCarrera.Text);
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@Id", txtId.Text);
+                cmd.Parameters.AddWithValue("@Calificacion", Aciertos);
+                cmd.Parameters.AddWithValue("@Fecha", DateTime.Now.ToString("MM/dd/yyyy"));
+                cmd.ExecuteNonQuery();
+
+                //Se imprime en consola los datos almadenados
+                RetrieveMultipleResults(conn);
+            }
+            catch (Exception ex)
+            {
+                var error = ex.Message;
+                //Operador Ternario; es igual a utilizar un if y un else
+                MessageBox.Show((ex is SqlException) ? ($"SQL error is: {error}!") : ($"Unexpected Error: {error}!"), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                ConnectionDB.DisconnectDataBase(conn);
+            }
+
+            //Limpiar todos los campos
+            foreach (StackPanel s in FindVisualChildren<StackPanel>(window))
+            {
+                foreach (RadioButton rb in s.Children)
+                {
+                    rb.IsChecked = false;
+                    Aciertos=0;
+                    Errores = 0;
+                }
+            }
+
+            foreach (TextBox txt in FindVisualChildren<TextBox>(window))
+            {
+                txt.Text = "";
+            }
+        }
+
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        static void RetrieveMultipleResults(OleDbConnection connection)
+        {
+            using (connection)
+            {
+                OleDbCommand command = new OleDbCommand(
+                  "SELECT * FROM Calificaciones",
+                  connection);
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                OleDbDataReader reader = command.ExecuteReader();
+
+                while (reader.HasRows)
+                {
+                    Console.WriteLine("\t{0}\t{1}\t{2}", reader.GetName(0),
+                        reader.GetName(1), reader.GetName(2));
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}\t{2}", reader.GetInt32(0),
+                            reader.GetString(1), reader.GetString(2));
+                    }
+                    reader.NextResult();
+                }
+            }
         }
     }
 }
